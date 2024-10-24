6b1dd5d7a0e8972045fbb24704feaa50773266c9
Shawn_Liu940526 <shawn.liu19940526@gmail.com>
2024-10-24 10:02:55 +0800

    resolve warning

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.scss';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"> 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
